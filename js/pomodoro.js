43304e240efc790b919ce91668ad19bacb1eccd2
Aboshady <aboshady48585@gmail.com>
2026-06-11 00:46:28 +0300

    timer

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -18,5 +18,42 @@ function updateDisplay() {
     let hrs = Math.floor(totalSecondsLeft / 3600);
     let mins = Math.floor((totalSecondsLeft % 3600) / 60);
     let secs = totalSecondsLeft % 60;
-    
+    if (hrs > 0) {
+        hrDisplay.classList.remove('hidden');
+        hrColon.classList.remove('hidden');
+        hrDisplay.textContent = hrs < 10 ? '0' + hrs : hrs;
+    } else {
+        hrDisplay.classList.add('hidden');
+        hrColon.classList.add('hidden');
+    }
+    minDisplay.textContent = mins < 10 ? '0' + mins : mins;
+    secDisplay.textContent = secs < 10 ? '0' + secs : secs;
 }
+
+function startTimer() {
+    if (countdownInterval !== null) return;
+    startBtn.classList.add('hidden');
+    pauseBtn.classList.remove('hidden');
+    countdownInterval = setInterval(() => {
+        if (totalSecondsLeft > 0) {
+            totalSecondsLeft--;
+            updateDisplay();
+        } else {
+            clearInterval(countdownInterval);
+            countdownInterval = null;
+            const alertSound = document.getElementById('alarm-sound');
+            if (alertSound) alertSound.play();
+            alert("Session is over");
+            resetTimer();
+        }
+    }, 1000);
+}
+
+function pauseTimer() {
+    clearInterval(countdownInterval);
+    countdownInterval = null;
+    pauseBtn.classList.add('hidden');
+    startBtn.classList.remove('hidden');
+}
+
+function
